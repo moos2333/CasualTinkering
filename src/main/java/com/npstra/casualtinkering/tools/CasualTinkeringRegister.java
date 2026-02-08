921ecc16b53197ce2607cc7c02c42881da82f109
moos2333 <xvruikai0804@126.com>
2026-02-08 14:05:30 +0800

    Update CasualTinkeringRegister.java

--- src/main/java/com/npstra/casualtinkering/tools/CasualTinkeringRegister.java
+++ src/main/java/com/npstra/casualtinkering/tools/CasualTinkeringRegister.java
@@ -21,24 +21,24 @@ public class CasualTinkeringRegister {
 
     @SideOnly(Side.CLIENT)
     private static void registerModifierModels() {
-        registerModifierModel(TinkerModifiers.modSharpness, new ResourceLocation("casual_tinkering:models/item/modifiers/sharpness"));
-        registerModifierModel(TinkerModifiers.modDiamond, new ResourceLocation("casual_tinkering:models/item/modifiers/diamond"));
-        registerModifierModel(TinkerModifiers.modEmerald, new ResourceLocation("casual_tinkering:models/item/modifiers/emerald"));
-        registerModifierModel(TinkerModifiers.modHaste, new ResourceLocation("casual_tinkering:models/item/modifiers/haste"));
-        registerModifierModel(TinkerModifiers.modFiery, new ResourceLocation("casual_tinkering:models/item/modifiers/fiery"));
-        registerModifierModel(TinkerModifiers.modKnockback, new ResourceLocation("casual_tinkering:models/item/modifiers/knockback"));
-        registerModifierModel(TinkerModifiers.modBaneOfArthopods, new ResourceLocation("casual_tinkering:models/item/modifiers/bane_of_arthopods"));
-        registerModifierModel(TinkerModifiers.modBeheading, new ResourceLocation("casual_tinkering:models/item/modifiers/beheading"));
-        registerModifierModel(TinkerModifiers.modGlowing, new ResourceLocation("casual_tinkering:models/item/modifiers/glowing"));
-        registerModifierModel(TinkerModifiers.modLuck, new ResourceLocation("casual_tinkering:models/item/modifiers/luck"));
-        registerModifierModel(TinkerModifiers.modMendingMoss, new ResourceLocation("casual_tinkering:models/item/modifiers/mending_moss"));
-        registerModifierModel(TinkerModifiers.modNecrotic, new ResourceLocation("casual_tinkering:models/item/modifiers/necrotic"));
-        registerModifierModel(TinkerModifiers.modReinforced, new ResourceLocation("casual_tinkering:models/item/modifiers/reinforced"));
-        registerModifierModel(TinkerModifiers.modShulking, new ResourceLocation("casual_tinkering:models/item/modifiers/shulking"));
-        registerModifierModel(TinkerModifiers.modSilktouch, new ResourceLocation("casual_tinkering:models/item/modifiers/silktouch"));
-        registerModifierModel(TinkerModifiers.modSmite, new ResourceLocation("casual_tinkering:models/item/modifiers/smite"));
-        registerModifierModel(TinkerModifiers.modSoulbound, new ResourceLocation("casual_tinkering:models/item/modifiers/soulbound"));
-        registerModifierModel(TinkerModifiers.modWebbed, new ResourceLocation("casual_tinkering:models/item/modifiers/webbed"));
+        registerModifierModel(TinkerModifiers.modSharpness, new ResourceLocation("casualtinkering:models/item/modifiers/sharpness"));
+        registerModifierModel(TinkerModifiers.modDiamond, new ResourceLocation("casualtinkering:models/item/modifiers/diamond"));
+        registerModifierModel(TinkerModifiers.modEmerald, new ResourceLocation("casualtinkering:models/item/modifiers/emerald"));
+        registerModifierModel(TinkerModifiers.modHaste, new ResourceLocation("casualtinkering:models/item/modifiers/haste"));
+        registerModifierModel(TinkerModifiers.modFiery, new ResourceLocation("casualtinkering:models/item/modifiers/fiery"));
+        registerModifierModel(TinkerModifiers.modKnockback, new ResourceLocation("casualtinkering:models/item/modifiers/knockback"));
+        registerModifierModel(TinkerModifiers.modBaneOfArthopods, new ResourceLocation("casualtinkering:models/item/modifiers/bane_of_arthopods"));
+        registerModifierModel(TinkerModifiers.modBeheading, new ResourceLocation("casualtinkering:models/item/modifiers/beheading"));
+        registerModifierModel(TinkerModifiers.modGlowing, new ResourceLocation("casualtinkering:models/item/modifiers/glowing"));
+        registerModifierModel(TinkerModifiers.modLuck, new ResourceLocation("casualtinkering:models/item/modifiers/luck"));
+        registerModifierModel(TinkerModifiers.modMendingMoss, new ResourceLocation("casualtinkering:models/item/modifiers/mending_moss"));
+        registerModifierModel(TinkerModifiers.modNecrotic, new ResourceLocation("casualtinkering:models/item/modifiers/necrotic"));
+        registerModifierModel(TinkerModifiers.modReinforced, new ResourceLocation("casualtinkering:models/item/modifiers/reinforced"));
+        registerModifierModel(TinkerModifiers.modShulking, new ResourceLocation("casualtinkering:models/item/modifiers/shulking"));
+        registerModifierModel(TinkerModifiers.modSilktouch, new ResourceLocation("casualtinkering:models/item/modifiers/silktouch"));
+        registerModifierModel(TinkerModifiers.modSmite, new ResourceLocation("casualtinkering:models/item/modifiers/smite"));
+        registerModifierModel(TinkerModifiers.modSoulbound, new ResourceLocation("casualtinkering:models/item/modifiers/soulbound"));
+        registerModifierModel(TinkerModifiers.modWebbed, new ResourceLocation("casualtinkering:models/item/modifiers/webbed"));
     }
 
     @SubscribeEvent
