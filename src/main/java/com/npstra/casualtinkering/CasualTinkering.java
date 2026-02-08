0f45384e47c0bde2c144f1b13adf2fe12c690741
moos2333 <xvruikai0804@126.com>
2026-02-08 14:20:49 +0800

    updates

--- src/main/java/com/npstra/casualtinkering/CasualTinkering.java
+++ src/main/java/com/npstra/casualtinkering/CasualTinkering.java
@@ -15,7 +15,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class CasualTinkering {
     public static final String MODID = "casualtinkering";
     public static final String NAME = "Casual Tinkering";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "0.0.1";
 
     @SidedProxy(clientSide = "com.npstra.casualtinkering.client.CasualTinkeringClientProxy",
             serverSide = "com.npstra.casualtinkering.common.CasualTinkeringCommonProxy")
