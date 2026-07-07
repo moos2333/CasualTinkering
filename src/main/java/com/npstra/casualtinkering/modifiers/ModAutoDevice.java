f9ab08fc02e4c0467d221fbd99291eb118758528
moos2333 <xvruikai0804@126.com>
2026-07-08 00:34:58 +0800

    v0.1.12 update

--- src/main/java/com/npstra/casualtinkering/modifiers/ModAutoDevice.java
+++ src/main/java/com/npstra/casualtinkering/modifiers/ModAutoDevice.java
@@ -79,7 +79,7 @@ public class ModAutoDevice extends ModifierTrait {
 
         AxisAlignedBB aabb = new AxisAlignedBB(player.getPosition()).grow(RADIUS);
         List<EntityLivingBase> targets = world.getEntitiesWithinAABB(EntityLivingBase.class, aabb,
-                e -> (e instanceof IMob || e.getRevengeTarget() == player));
+                e -> e != player && (e instanceof IMob || e.getRevengeTarget() == player));
 
         if (targets.isEmpty()) return;
 
